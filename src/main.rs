fbc69a257bd76e655c28f12ab6c114014cfb1521
Jakub Konka <jakub.konka@strath.ac.uk>
2015-11-24 14:47:51 +0000

    Write output to file

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate docopt;
 extern crate gitrev;
 
 use docopt::Docopt;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::fs::File;
 
 const USAGE: &'static str = "
@@ -31,6 +31,8 @@ fn main() {
     let args: Args = Docopt::new(USAGE)
                             .and_then(|d| d.decode())
                             .unwrap_or_else(|e| e.exit());
+
+    // Get all the necessary data from git and system
     let curr_rev = gitrev::git_describe().unwrap_or_else(|e| {
         println!("{}", e);
         ::std::process::exit(1);
@@ -52,8 +54,16 @@ fn main() {
     println!("Current branch: {}", curr_branch);
     println!("Remote url: {}", remote_url);
 
+    // Read in contents of the template file
     let mut f = File::open(&args.arg_src_version_file).unwrap();
     let mut contents = String::new();
     f.read_to_string(&mut contents).unwrap();
     println!("Template contents:\n{}", contents);
+
+    // Parse the contents in search of special delimiters
+    // and replace with the git data
+
+    // Write contents to the destination file
+    f = File::create(&args.arg_dst_version_file).unwrap();
+    f.write_all(&contents.into_bytes()).unwrap();
 }

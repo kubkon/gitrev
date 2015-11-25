432920857a8cac9ba0b637e532055ea84bb01e4a
Jakub Konka <kubkon@jakubkonka.com>
2015-11-25 16:50:07 +0000

    Create container struct and handle file io errors

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,16 @@ extern crate time;
 use std::process::Command;
 use std::error::Error;
 use std::fmt;
+use std::io::{Read, Write};
+use std::fs::File;
 
-use self::GitRevError::{Git, DateTime};
-
-pub type GitRevResult = Result<String, String>;
+use self::GitRevError::{Git, DateTime, FileIO};
 
 #[derive(Debug)]
 pub enum GitRevError {
     Git(String),
     DateTime(String),
+    FileIO(String),
 }
 
 impl GitRevError {
@@ -27,6 +28,7 @@ impl fmt::Display for GitRevError {
         match *self {
             Git(ref s) => write!(f, "git error: {}", s),
             DateTime(ref s) => write!(f, "system time error: {}", s),
+            FileIO(ref s) => write!(f, "file IO error: {}", s),
         }
     }
 }
@@ -36,28 +38,92 @@ impl Error for GitRevError {
         match *self {
             Git(..) => "git command failed",
             DateTime(..) => "failed to parse system time",
+            FileIO(..) => "file input-output error",
         }
     }
 }
 
-pub fn git_command(cmd: &str) -> Result<String, GitRevError> {
-    // Parse args
-    let args: Vec<&str> = cmd.split_whitespace().collect();
-    let output = match Command::new("git").args(&args).output() {
-        Err(e) => return Err(Git(String::from(e.description()))),
-        Ok(out) => out,
-    };
-    if output.stdout.is_empty() {
-        let err_msg = String::from_utf8_lossy(&output.stderr);
-        return Err(Git(String::from(err_msg.trim())));
-    }
-    let result = String::from_utf8_lossy(&output.stdout);
-    Ok(String::from(result.trim()))
+#[derive(Clone, Debug)]
+pub struct GitRev {
+    template_filename: String,
+    generated_filename: String,
 }
 
-pub fn build_time() -> Result<String, GitRevError> {
-    match time::strftime("%F %T %Z", &time::now_utc()) {
-        Err(e) => Err(DateTime(String::from(e.description()))),
-        Ok(out) => Ok(out),
+impl GitRev {
+    pub fn new(template_filename: &str, generated_filename: &str) -> GitRev {
+        GitRev {
+            template_filename: String::from(template_filename),
+            generated_filename: String::from(generated_filename),
+        }
+    }
+
+    pub fn generate_from_template(&self) -> Result<(), GitRevError> {
+        // Get all the necessary data from git and system
+        let curr_rev = try!(self.git_command("describe --always"));
+        let curr_branch = try!(self.git_command("rev-parse --abbrev-ref HEAD"));
+        let remote_url = try!(self.git_command("config --get remote.origin.url"));
+        let build_time = try!(self.build_time());
+        println!("Current revision: {}", curr_rev);
+        println!("Build time: {}", build_time);
+        println!("Current branch: {}", curr_branch);
+        println!("Remote url: {}", remote_url);
+
+        // Read in contents of the template file
+        let mut contents = try!(self.read_in_template_file());
+
+        // Parse the contents in search of special delimiters
+        // and replace with the git data
+        contents = contents.replace("$WCREV$", &curr_rev)
+                           .replace("$WCNOW$", &build_time)
+                           .replace("$WCURL$", &remote_url);
+
+        // Write contents to the destination file
+        self.write_to_dst_file(&contents)
+    }
+
+    fn git_command(&self, cmd: &str) -> Result<String, GitRevError> {
+        // Parse args
+        let args: Vec<&str> = cmd.split_whitespace().collect();
+        let output = match Command::new("git").args(&args).output() {
+            Err(e) => return Err(Git(String::from(e.description()))),
+            Ok(out) => out,
+        };
+        if output.stdout.is_empty() {
+            let err_msg = String::from_utf8_lossy(&output.stderr);
+            return Err(Git(String::from(err_msg.trim())));
+        }
+        let result = String::from_utf8_lossy(&output.stdout);
+        Ok(String::from(result.trim()))
+    }
+
+    fn build_time(&self) -> Result<String, GitRevError> {
+        match time::strftime("%F %T %Z", &time::now_utc()) {
+            Err(e) => Err(DateTime(String::from(e.description()))),
+            Ok(out) => Ok(out),
+        }
+    }
+
+    fn read_in_template_file(&self) -> Result<String, GitRevError> {
+        let mut f = match File::open(&self.template_filename) {
+            Err(e) => return Err(FileIO(String::from(e.description()))),
+            Ok(f) => f,
+        };
+        let mut contents = String::new();
+        match f.read_to_string(&mut contents) {
+            Err(e) => return Err(FileIO(String::from(e.description()))),
+            Ok(_) => (),
+        };
+        Ok(contents)
+    }
+
+    fn write_to_dst_file(&self, contents: &str) -> Result<(), GitRevError> {
+        let mut f = match File::create(&self.generated_filename) {
+            Err(e) => return Err(FileIO(String::from(e.description()))),
+            Ok(f) => f,
+        };
+        match f.write_all(contents.as_bytes()) {
+            Err(e) => Err(FileIO(String::from(e.description()))),
+            Ok(()) => Ok(()),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,14 @@ extern crate docopt;
 extern crate gitrev;
 
 use docopt::Docopt;
-use std::io::{Read, Write};
-use std::fs::File;
+use gitrev::GitRev;
 
 const USAGE: &'static str = "
 GITREV is a utility program that is meant to serve as a replacement for
 SubWCRev.exe.
 
 Usage:
-    gitrev.exe <src-version-file> <dst-version-file>
+    gitrev.exe <template-filename> <generated-filename>
     gitrev.exe (-h | --help)
     gitrev.exe --version
 
@@ -22,8 +21,8 @@ Options:
 
 #[derive(Debug, RustcDecodable)]
 struct Args {
-    arg_src_version_file: String,
-    arg_dst_version_file: String,
+    arg_template_filename: String,
+    arg_generated_filename: String,
 }
 
 fn main() {
@@ -32,32 +31,6 @@ fn main() {
                             .and_then(|d| d.decode())
                             .unwrap_or_else(|e| e.exit());
 
-    // Get all the necessary data from git and system
-    let curr_rev = gitrev::git_command("describe --always")
-                          .unwrap_or_else(|e| e.exit());
-    let curr_branch = gitrev::git_command("rev-parse --abbrev-ref HEAD")
-                             .unwrap_or_else(|e| e.exit());
-    let remote_url = gitrev::git_command("config --get remote.origin.url")
-                            .unwrap_or_else(|e| e.exit());
-    let build_time = gitrev::build_time()
-                            .unwrap_or_else(|e| e.exit());
-    println!("Current revision: {}", curr_rev);
-    println!("Build time: {}", build_time);
-    println!("Current branch: {}", curr_branch);
-    println!("Remote url: {}", remote_url);
-
-    // Read in contents of the template file
-    let mut f = File::open(&args.arg_src_version_file).unwrap();
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).unwrap();
-
-    // Parse the contents in search of special delimiters
-    // and replace with the git data
-    contents = contents.replace("$WCREV$", &curr_rev)
-                       .replace("$WCNOW$", &build_time)
-                       .replace("$WCURL$", &remote_url);
-
-    // Write contents to the destination file
-    f = File::create(&args.arg_dst_version_file).unwrap();
-    f.write_all(&contents.into_bytes()).unwrap();
+    let gr = GitRev::new(&args.arg_template_filename, &args.arg_generated_filename);
+    gr.generate_from_template().unwrap_or_else(|e| e.exit());
 }
